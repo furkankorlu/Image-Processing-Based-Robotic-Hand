76a8d9feef21304495fdbefb48bb5581b748bab9
FurkanKorlu <furkankorlu46@gmail.com>
2023-09-08 20:19:32 +0300

    Webcamden görüntüyü alma Mediapipe classlarını alma

--- degressdetection.py
+++ degressdetection.py
@@ -2,3 +2,9 @@ import mediapipe as mp
 import cv2 as cv
 import numpy as np
 from cvzone.SerialModule import SerialObject
+
+cap = cv.VideoCapture(0)
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
